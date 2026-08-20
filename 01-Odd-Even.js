dfdaa5063d4bc22591f2c9180826de386d55a11e
ashwiniperumalsamyk-cmd <ashwiniperumalsamyk@gmail.com>
2026-08-20 15:41:22 +0530

    Add files via upload
    01-Odd Even JS code

---
+++ 01-Odd-Even.js
@@ -0,0 +1,12 @@
+function isOddEven(number){
+    if (number % 2 === 0 ){
+        return "Even"
+    }else {
+        return "Odd";
+
+        }
+    }
+    let num = 8 //Decalred and initialize the variable
+    console.log(isOddEven(num));//call the function and print the result
+
+
