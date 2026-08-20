37430e6a714dc883d12b357fc44d8e87f6115fc6
ashwiniperumalsamyk-cmd <ashwiniperumalsamyk@gmail.com>
2026-08-20 16:08:41 +0530

    Add files via upload
    03-Var-let-const JS Code

---
+++ 03-var-let-const.js
@@ -0,0 +1,20 @@
+const browserName = "Chrome";
+funstion getBrowserName(){
+    if (browserName === "Chrome"){
+        var browserName = "chrome";//using var first
+    }
+    console.log(browserName);
+}
+getBrowserName();
+
+
+
+const browserName = "Chrome";
+function getBrowserName(){
+   if (browserName === "Chrome"){
+       let browserName = "Chrome"; // convert var into let
+
+    }
+    console.log(browserName);
+}
+getBrowserName();
